e1a855beca2f79a537c69b2fbcb07b54ae97c960
Jorge <jorge-padron99@hotmail.com>
2022-03-21 11:18:52 +0100

    Añadiendo componentes a la vista

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,24 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ShowViajeComponent } from './show-viaje/show-viaje.component';
 
+import {MatIconModule} from '@angular/material/icon'; 
+import {MatCardModule} from '@angular/material/card'; 
+import { MatButtonModule} from '@angular/material/button';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ShowViajeComponent,
+    
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatCardModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/show-viaje/show-viaje.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-show-viaje',
+  templateUrl: './show-viaje.component.html',
+  styleUrls: ['./show-viaje.component.css'],
+})
+export class ShowViajeComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}

d01893d0edffbaa101f03fca31c21722cdc59862
hugobanola99 <hugo.banola101@alu.ulpgc.es>
2022-03-27 13:39:12 +0100

    Interfaz Crear Viaje V2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,15 @@ import { ReactiveFormsModule } from "@angular/forms";
 import { MatFormFieldModule } from "@angular/material/form-field";
 import { MatInputModule } from "@angular/material/input";
 import { MatButtonModule } from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon'
+import { FormsModule } from '@angular/forms';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatDatepickerModule} from '@angular/material/datepicker';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ViajeComponent } from './viaje/viaje.component';
+
 
 @NgModule({
   declarations: [
@@ -20,7 +25,12 @@ import { ViajeComponent } from './viaje/viaje.component';
     ReactiveFormsModule,
     MatFormFieldModule,
     MatInputModule,
-    MatButtonModule
+    MatButtonModule,
+    MatIconModule,
+    FormsModule,
+    MatToolbarModule,
+    MatDatepickerModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/viaje/viaje.component.ts
+++ src/app/viaje/viaje.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl } from "@angular/forms";
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore/';
+import {MatDialog} from '@angular/material/dialog';
+import {MatToolbarModule} from '@angular/material/toolbar';
 
 @Component({
   selector: 'app-viaje',
@@ -14,6 +17,16 @@ export class ViajeComponent implements OnInit {
 
   ngOnInit(): void {
     this.inputControl = new FormControl();
+    
   }
+
+  
+
+  value=""
+  clearValue() {
+   this.value="";
+  }
+
+
 
 }

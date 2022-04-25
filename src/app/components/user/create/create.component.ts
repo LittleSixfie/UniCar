42bde36d8f563185b3d4b7468d76af7022f141d1
Jorge-Marrero <jmc1161199@gmail.com>
2022-04-25 17:23:15 +0100

    Authentification

--- src/app/components/user/create/create.component.ts
+++ src/app/components/user/create/create.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { getAuth, createUserWithEmailAndPassword } from '@firebase/auth';
 import { User } from 'src/app/models/user';
 import { CrudUserService } from 'src/app/services/crud-user.service';
 
@@ -12,18 +13,43 @@ export class CreateComponent implements OnInit {
   user: User = new User();
   submitted = false;
   hide = true;
+  auth = getAuth()
 
   constructor(private crudUserService: CrudUserService) {}
 
   ngOnInit(): void {}
 
   saveUser(): void {
-    this.crudUserService.create(this.user)
+    if(this.registerUser()){
+      this.crudUserService.create(this.user)
+    }
   }
 
-  newUser(): void {
-    this.submitted = false;
-    this.user = new User;
+  private registerUser(): boolean {
+    /*createUserWithEmailAndPassword(this.auth, this.user.userEmail, this.user.userPassword)
+    .then((userCredentials) => {
+    
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+    });*/
+    return true;
+  }
+
+  public searchFile() {
+    var input = document.createElement('input')
+    input.type = 'file'
+    input.accept = 'image/png, image/jpeg, image/jpg'
+    input.onchange = (e) => {
+      if((<HTMLInputElement>e.target).files != null){
+        var file = (<HTMLInputElement>e.target).files?.[0];
+        this.user.userDriverLicense = file?.
+      }
+
+    }
+    console.log(this.user.userDriverLicense);
+    input.click()
   }
 
 }

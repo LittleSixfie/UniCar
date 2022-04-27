1cc7e5e8b4c5a2874d491f4a067486a4c2dab837
albertomejias99 <albertito.mejias@gmail.com>
2022-04-27 20:23:17 +0100

    Update User front done

--- src/app/components/user/update/update.component.ts
+++ src/app/components/user/update/update.component.ts
@@ -1,4 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
+import { User } from 'src/app/models/user';
+import { Trip } from 'src/app/models/trips.model';
+import { CrudUserService } from 'src/app/services/crud-user.service';
+import { DocumentReference } from 'firebase/firestore';
+import { DocumentData } from 'rxfire/firestore/interfaces';
+import { ActivatedRoute, Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-update',
@@ -7,9 +15,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UpdateComponent implements OnInit {
 
-  constructor() { }
+  emailFormControl = new FormControl('', [Validators.required, Validators.email]);
+
+  public inputControl: FormControl = new FormControl();
+
+  user: User = new User();
+  constructor( private crudUser : CrudUserService, private activatedRoute: ActivatedRoute, private router: Router) { }
 
   ngOnInit(): void {
+    this.inputControl = new FormControl();
   }
 
+  updateTrip(){
+    this.router.navigate(['/updateTrip'])
+  }
+
+  onUpdate(){
+    this.crudUser.update(this.activatedRoute.snapshot.params['id'], this.user);
+  }
+
+
+  usernameNotNull(username:string){
+    return username.length!=0;
+  }
 }

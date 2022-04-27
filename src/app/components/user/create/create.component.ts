f08b0c0939ab9f0a8061a63633d58fd4d9e34c58
Jorge-Marrero <jmc1161199@gmail.com>
2022-04-27 18:25:59 +0100

    Upload License and picture

--- src/app/components/user/create/create.component.ts
+++ src/app/components/user/create/create.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { getAuth, createUserWithEmailAndPassword } from '@firebase/auth';
 import { User } from 'src/app/models/user';
 import { CrudUserService } from 'src/app/services/crud-user.service';
+import { getStorage, ref, uploadBytes } from 'firebase/storage'
 
 @Component({
   selector: 'app-create',
@@ -13,7 +14,10 @@ export class CreateComponent implements OnInit {
   user: User = new User();
   submitted = false;
   hide = true;
+  License?: File
+  Picture?: File
   auth = getAuth()
+  storage = getStorage()
 
   constructor(private crudUserService: CrudUserService) {}
 
@@ -21,6 +25,7 @@ export class CreateComponent implements OnInit {
 
   saveUser(): void {
     if(this.registerUser()){
+      this.uploadPictures()
       this.crudUserService.create(this.user)
     }
   }
@@ -37,19 +42,45 @@ export class CreateComponent implements OnInit {
     return true;
   }
 
-  public searchFile() {
+  public searchLicense() {
     var input = document.createElement('input')
     input.type = 'file'
     input.accept = 'image/png, image/jpeg, image/jpg'
     input.onchange = (e) => {
       if((<HTMLInputElement>e.target).files != null){
-        var file = (<HTMLInputElement>e.target).files?.[0];
-        this.user.userDriverLicense = file?.
+        this.License = (<HTMLInputElement>e.target).files?.[0];
+        this.user.userDriverLicense = this.License?.name
       }
 
     }
     console.log(this.user.userDriverLicense);
     input.click()
+  }
+
+  public searchPicture(){
+    var input = document.createElement('input')
+    input.type = 'file'
+    input.accept = 'image/png, image/jpeg, image/jpg'
+    input.onchange = (e) => {
+      if((<HTMLInputElement>e.target).files != null){
+        this.Picture = (<HTMLInputElement>e.target).files?.[0];
+        this.user.userPic = this.Picture?.name
+      }
+
+    }
+    console.log(this.user.userDriverLicense);
+    input.click()
+  }
+
+  private uploadPictures(){
+    if(this.License !=  null){
+      const licenseRef = ref(this.storage, `${this.user.userEmail}/License/${this.user.userDriverLicense}`)
+      uploadBytes(licenseRef, this.License)
+    }
+    if(this.Picture != null){
+      const pictureRef = ref(this.storage, `${this.user.userEmail}/Picture/${this.user.userPic}`)
+      uploadBytes(pictureRef, this.Picture)
+    }
   }
 
 }

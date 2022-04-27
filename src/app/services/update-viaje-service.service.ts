713de305a973f5f5e951f77312463826f422beb2
hugobanola99 <hugo.banola101@alu.ulpgc.es>
2022-04-27 18:16:15 +0100

    Update Viaje Terminado

---
+++ src/app/services/update-viaje-service.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { Firestore } from '@angular/fire/firestore';
+import { Router } from '@angular/router';
+import { doc, setDoc } from 'firebase/firestore';
+import { Trip } from '../models/trips.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UpdateViajeServiceService {
+
+  db : Firestore;
+
+  constructor(db: Firestore, private router:Router) {
+    this.db = db;
+   
+  }
+
+  updateTrip(trip: Trip, id: string){
+    const tripDocRef = doc(this.db, 'trips/'+id);
+    setDoc(tripDocRef,trip);
+    this.router.navigate(["viaje/" + id])
+  }
+}

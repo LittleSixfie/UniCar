43a1d449c07c61de749d6a8b24f6a333a64a5e81
albertomejias99 <albertito.mejias@gmail.com>
2022-05-02 09:04:50 +0100

    backup commit update user

--- src/app/components/user/update/update.component.ts
+++ src/app/components/user/update/update.component.ts
@@ -3,9 +3,11 @@ import { FormControl, Validators } from '@angular/forms';
 import { User } from 'src/app/models/user';
 import { Trip } from 'src/app/models/trips.model';
 import { CrudUserService } from 'src/app/services/crud-user.service';
+import { ShowViajeService } from 'src/app/services/show-viaje-service.service';
 import { DocumentReference } from 'firebase/firestore';
 import { DocumentData } from 'rxfire/firestore/interfaces';
 import { ActivatedRoute, Router } from '@angular/router';
+import { ReadComponent } from '../read/read.component';
 
 
 @Component({
@@ -14,28 +16,33 @@ import { ActivatedRoute, Router } from '@angular/router';
   styleUrls: ['./update.component.css']
 })
 export class UpdateComponent implements OnInit {
+  id: string;
+  userData?: User;
+  userCreatedTrips?: string[];
+  userRequestedTrips?: string[];
+  readComponent: ReadComponent;
 
+  constructor(private crudUserService: CrudUserService, private showViajeService: ShowViajeService, private router: ActivatedRoute) {
+    this.userData = new User();
+    this.id = this.router.snapshot.params['id'];
+    this.readComponent = new ReadComponent(crudUserService, showViajeService, router);
+  }
   emailFormControl = new FormControl('', [Validators.required, Validators.email]);
 
   public inputControl: FormControl = new FormControl();
 
   user: User = new User();
-  constructor( private crudUser : CrudUserService, private activatedRoute: ActivatedRoute, private router: Router) { }
 
   ngOnInit(): void {
     this.inputControl = new FormControl();
   }
 
-  updateTrip(){
-    this.router.navigate(['/updateTrip'])
+  onUpdate() {
+    this.crudUserService.update(this.router.snapshot.params['id'], this.user);
   }
 
-  onUpdate(){
-    this.crudUser.update(this.activatedRoute.snapshot.params['id'], this.user);
+  public update(): void {
+    if (this.userData != undefined) this.crudUserService.update(this.id, this.userData);
   }
 
-
-  usernameNotNull(username:string){
-    return username.length!=0;
-  }
 }
